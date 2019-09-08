5f65a643a8de38608954dc9967882db5c09d0cde
Mike Zamansky <zamansky@gmail.com>
2019-09-07 20:25:44 -0400

    Z

--- src/main.cljs
+++ src/main.cljs
@@ -1,13 +1,18 @@
 (ns main
   (:require [reagent.core :as r]
             [cljs.core.async :refer (chan put! <! go go-loop timeout)]
+            [for-editor :as e]
             ))
 
-
-
+(defn zeditor []
+  [e/Editor]
+  )
 (defn main-component []
   [:div 
-   [:h1 "This is a component"]
+   [:hr]
+   [zeditor ]
+   [:hr]
+
    ])
 
 
@@ -16,9 +21,9 @@
   )
 
 (defn reload! []
-  (mount main-component)
-  (print "Hello reload!"))
+(mount main-component)
+(print "Hello reload!"))
 
 (defn main! []
-  (mount main-component)
-  (print "Hello Main"))
+(mount main-component)
+(print "Hello Main"))
